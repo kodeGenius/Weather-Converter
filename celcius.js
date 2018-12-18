e265f8e395f799a544c6427b05d0d76ed1ff5a0d
Project <oladosu777@gmail.com>
2018-12-18 21:41:07 +0100

    new

---
+++ celcius.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+			// declare kelvin
+			//let kelvin = 250;
+	function cels() {
+				/*kelvin= e.target.value("check");
+				const celsius = kelvin - 273;
+				return celsius;*/
+				alert('celsius');
+	}
+				
+			// this calc celsius which is 273 less than kelvin value
+			
+</script>>
